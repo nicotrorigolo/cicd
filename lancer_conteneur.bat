2c992d7678de2d20a3c139751832d4a36dfd9d5c
nicotrorigolo <ndfndf78@gmail.com>
2023-03-31 03:37:04 +0200

    ajustement

--- lancer_conteneur.bat
+++ lancer_conteneur.bat
@@ -1,3 +1,3 @@
 call docker build -t nono %WORKSPACE_CICD%\container
-call docker run -v %WORKSPACE_CICD%:/home -it nono bash
+call docker run -p 127.0.0.1:5000:5000 -v %WORKSPACE_CICD%:/home -it nono bash
 
